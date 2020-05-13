1d1e1990f72d559717a884d78bc71986eaf4c8de
Mackenzie Browne <info@mackbrowne.com>
2020-05-13 22:08:14 +0200

    Add / Remove Functionality

--- src/EmailsInput/index.js
+++ src/EmailsInput/index.js
@@ -1,7 +1,86 @@
 import EmailsInputTemplate from './EmailsInput.html';
+import keycode from 'keycode';
+import randomEmail from 'random-email';
 
-export default (node, options = {}) => {
-  node.innerHTML = EmailsInputTemplate;
+export default class {
+  constructor(node, options = {}) {
+    this.node = node;
+    this.options = options;
+    this.node.innerHTML = EmailsInputTemplate;
 
-  node.querySelector('');
-};
+    this.emails = [];
+
+    this.PillTemplate = this.node.querySelector('#emails-input--pill-template');
+    this.ButtonTemplate = this.node.querySelector(
+      '#emails-input--button-template'
+    );
+
+    this.textAreaElement = this.node.querySelector('.emails-input--textarea');
+    this.pillsGroupElement = this.node.querySelector(
+      '.emails-input--pills-group'
+    );
+
+    this.inputElement = this.node.querySelector('input');
+    this.inputElement.addEventListener('keyup', (e) => this._keyPressEvent(e));
+    this.inputElement.addEventListener('paste', (e) => this._pasteEvent(e));
+    this.inputElement.addEventListener('blur', (e) => {
+      const textValue = this.inputElement.value;
+      if (textValue) this._addPill(textValue);
+    });
+
+    this.primaryButton = this.node.querySelector(
+      '.emails-input--button-primary'
+    );
+    this.primaryButton.addEventListener('click', () =>
+      this._addPill(randomEmail())
+    );
+
+    this.secondaryButton = this.node.querySelector(
+      '.emails-input--button-secondary'
+    );
+    this.secondaryButton.addEventListener('click', () => this._getCount());
+  }
+
+  _keyPressEvent(e) {
+    const key = keycode(e);
+    if (['enter', ','].includes(key)) {
+      this._addPill(this.inputElement.value.slice(0, -1));
+    }
+  }
+
+  _pasteEvent(e) {
+    e.preventDefault();
+    (e.clipboardData || window.clipboardData)
+      .getData('text')
+      .split(',')
+      .forEach((email) => this._addPill(email.trim()));
+  }
+
+  _addPill(email) {
+    this._clearInput();
+    this.emails.push(email);
+
+    const newPill = this.PillTemplate.content.cloneNode(true);
+
+    const pillElement = newPill.querySelector('.emails-input--pill');
+    newPill
+      .querySelector('img')
+      .addEventListener('click', () => this._removePill(pillElement));
+
+    newPill.querySelector('.emails-input--pill-label').innerHTML = email;
+
+    this.pillsGroupElement.appendChild(newPill);
+  }
+
+  _removePill(element) {
+    this.pillsGroupElement.removeChild(element);
+  }
+
+  _getCount() {
+    alert(`Total Emails: ${this.emails.length}`);
+  }
+
+  _clearInput() {
+    this.inputElement.value = '';
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import './style.scss';
 import EmailsInput from './EmailsInput';
 
 var inputContainerNode = document.querySelector('#emails-input');
-var emailsInput = EmailsInput(inputContainerNode);
+var emailsInput = new EmailsInput(inputContainerNode); 
+
+
+var inputContainerNode2 = document.querySelector('#emails-input2');
+var emailsInput = new EmailsInput(inputContainerNode2);
